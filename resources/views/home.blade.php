da2e1889f0f90fa2237b30d6118d56ed2c4ef9b1
Arif099757 <arahmanhakim026@gmail.com>
2024-12-16 12:09:05 +0700

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Home</title>
+    
+    <style>
+        body {
+    font-family: Arial, sans-serif;
+    background-color:  #6fc22c;
+    color: rgb(27, 26, 26);
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+}
+
+h1 {
+    color: #0066cc;
+}
+
+a {
+    text-decoration: none;
+    color: rgb(221, 167, 19);
+    background-color: #0066cc;
+    padding: 10px 20px;
+    border-radius: 5px;
+}
+
+a:hover {
+    background-color: #004999;
+}
+
+    </style>
+</head>
+<body>
+    <h1>Selamat Datang!</h1>
+    <p><strong>NIM:</strong>2257401013</p>
+    <p><strong>Nama:</strong>Arif Rahman Hakim</p>
+    <p><strong>Kelas:</strong>MI22B</p>
+    <a href="/logout">Logout</a>
+</body>
+</html>
